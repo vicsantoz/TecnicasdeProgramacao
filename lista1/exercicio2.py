76a06d6c70f9c204722327e63ee40ee468efd1e3
vicsantoz <142454650+vicsantoz@users.noreply.github.com>
2025-08-18 20:22:01 -0300

    LISTA 1

---
+++ lista1/exercicio2.py
@@ -0,0 +1,4 @@
+dolar = float(input("Qual valor, em dolares, deseja converter em reais?"))
+real = 3,12
+conversao = dolar * real
+print("O valor em reais sera de:" + conversao)
---
+++ lista1/exercício1.py
@@ -0,0 +1,4 @@
+real = float(input("Qual valor, em real, deseja converter em dolar?"))
+taxa = 3,12
+dolar = real / taxa
+print(f"R$ {real} equivale a US$ {dolar}")
